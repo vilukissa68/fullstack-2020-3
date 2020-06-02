cf4b48caf89f1ab8b2e7f47d3e1f450fbfa43ef5
vaino <vaino.granat1@gmail.com>
2020-06-02 17:47:48 +0300

    Added static frontend.

---
+++ build/precache-manifest.67a1ca62bba068a8ac020b815d8079ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6a4ed588dd74edd7a36c5d1cce62cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aab75ead549c86a9873a",
+    "url": "/static/css/main.b0a7673b.chunk.css"
+  },
+  {
+    "revision": "42850162ff5a3755ffef",
+    "url": "/static/js/2.5117b746.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5117b746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab75ead549c86a9873a",
+    "url": "/static/js/main.fb2e0ba0.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('body', function(req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
