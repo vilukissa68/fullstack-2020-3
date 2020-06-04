b09e2c748d72ef7463fb3abe654ca84886fad6fb
vaino <vaino.granat1@gmail.com>
2020-06-04 19:40:04 +0300

    Finished exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
+
+const Person = require('./models/person')
+
+let persons = []
 
 app.use(express.json())
 app.use(cors())
@@ -9,23 +14,6 @@ app.use(express.static('build'))
 morgan.token('body', function(req, res) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        name: "Arto Hellas",
-        number: "040-123456",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "39-44-532523",
-        id: 4
-    },
-    {
-        name: "Dan Abramov",
-        numbeid: "12-43-234345",
-        id: 3
-    },
-]
 
 const generateId = () => {
     return Math.floor(Math.random() * Math.floor(9999999))
@@ -39,14 +27,16 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(p => p.id === id)
     if (person){
-        res.json(person)
+       res.json(person) 
     }
     else {
         res.status(404).end()
@@ -81,18 +71,20 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
         number: body.number,
         id: generateId(),
-    }
+    })
 
     persons = persons.concat(person)
 
-    res.json(person)
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)}
 )
